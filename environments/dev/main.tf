74038ef5da988b65563396ebbd91b6ea8e2fc38a
malikpatan123 <malikpatans@gmail.com>
2025-06-26 13:56:02 +0530

    changes on ec2 updated ec2 ami south

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,4 +10,25 @@ module "vpc" {
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
   azs                 = ["ap-south-1a", "ap-south-1b"]
+} 
+
+module "public_ec2" {
+  source              = "../../modules/ec2"
+  ami_id              = "ami-0f918f7e67a3323f0"  # Use valid AMI for ap-south-1
+  instance_type       = "t2.micro"
+  subnet_id           = "subnet-06aa9f891c2080d02"
+  security_group_id   = module.vpc.demo_public_sg_id
+  associate_public_ip = true
+  name                = "public-server"
 }
+
+module "private_ec2" {
+  source              = "../../modules/ec2"
+  ami_id              = "ami-0f918f7e67a3323f0"
+  instance_type       = "t2.micro"
+  subnet_id           = "subnet-02b11ed43acb873c5"
+  security_group_id   = module.vpc.demo_private_sg_id
+  associate_public_ip = false
+  name                = "private-server"
+}
+
