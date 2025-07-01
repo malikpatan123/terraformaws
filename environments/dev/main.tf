a9ae9d2bbf1e89740eb4ad76be081d36c5ed9a99
malikpatan123 <malikpatans@gmail.com>
2025-07-01 20:56:55 +0530

    updated code

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
   private_subnet_cidr = "10.0.2.0/24"
   azs                 = ["ap-south-1a", "ap-south-1b"]
 } 
-
+/*
 module "public_ec2" {
   source              = "../../modules/ec2"
   ami_id              = "ami-0f918f7e67a3323f0"  # Use valid AMI for ap-south-1
@@ -21,7 +21,8 @@ module "public_ec2" {
   associate_public_ip = true
   name                = "public-server"
 }
-
+*/
+/*
 module "private_ec2" {
   source              = "../../modules/ec2"
   ami_id              = "ami-0f918f7e67a3323f0"
@@ -29,24 +30,36 @@ module "private_ec2" {
   subnet_id           = "subnet-02b11ed43acb873c5"
   security_group_id   = module.vpc.demo_private_sg_id
   associate_public_ip = false
+  key_name            = var.key_name
   name                = "private-server"
+  iam_instance_profile   = module.iam_role.instance_profile_name
 }
+*/
 
 module "iam_role" {
   source    = "../../modules/iam_role"
   role_name = "ec2-s3-access-role"
 }
 
-resource "aws_instance" "public_ec2" {
-  ami                    = "ami-0abcd1234abcd1234"
+module "public_ec2" {
+  source = "../../modules/ec2"
+
+  name                   = "public-instance"   # 🔹 Use lowercase "name"
+  ami_id                 = "ami-0f918f7e67a3323f0"
   instance_type          = "t2.micro"
-  subnet_id              = var.public_subnet_id
+  subnet_id              = module.vpc.public_subnet_id
+  security_group_id      = module.vpc.demo_public_sg_id
+  associate_public_ip    = true
   key_name               = var.key_name
   iam_instance_profile   = module.iam_role.instance_profile_name
-  associate_public_ip_address = true
 
-  tags = {
-    Name = "public-instance"
-  }
 }
-
+/*
+module "vpc_flow_logs" {
+  source         = "../../modules/vpc_flow_logs"
+  vpc_id         = "vpc-07d30b640033b9c46"  # Specific VPC ID
+  s3_bucket_arn  = "arn:aws:s3:::blackbucket-terraform-state"
+  traffic_type   = "ALL"
+  max_aggregation_interval = 60  # <-- this sets aggregation to 1 minute
+}
+*/
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -13,3 +13,13 @@ variable "key_name" {
   type        = string
 }
 
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
